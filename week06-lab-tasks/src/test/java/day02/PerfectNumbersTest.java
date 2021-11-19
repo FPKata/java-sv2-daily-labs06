a661c091dfb9f7ac5bc4b8e253582a8b6c8a1dd6
FPKata <kata.percze@gmail.com>
2021-11-19 11:47:11 +0100

    PerfectNumbers done

---
+++ week06-lab-tasks/src/test/java/day02/PerfectNumbersTest.java
@@ -0,0 +1,22 @@
+package day02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PerfectNumbersTest {
+
+    @Test
+    void testPerfectNumber(){
+        PerfectNumbers perfectNumbers = new PerfectNumbers();
+
+//        boolean result = perfectNumbers.isPerfectNumber(8128);
+//        assertEquals(true, result);  //assertTrue(result);
+
+        assertTrue(perfectNumbers.isPerfectNumber(8128));
+        assertTrue(perfectNumbers.isPerfectNumber(6));
+
+        assertFalse(perfectNumbers.isPerfectNumber(10));
+        assertFalse(perfectNumbers.isPerfectNumber(100));
+    }
+}
